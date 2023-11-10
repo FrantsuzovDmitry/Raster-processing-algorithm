28d95027c24bc86c9cc89a86417a337a11ab4286
Dmitry <francuzov_dm@mail.ru>
2023-11-10 16:32:28 +0700

    Final commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.selectImageButton = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.transformButton = new System.Windows.Forms.Button();
+			this.applyFilterButton = new System.Windows.Forms.Button();
+			this.filtersBox = new System.Windows.Forms.ComboBox();
+			this.primaryAlgorythmButtom = new System.Windows.Forms.RadioButton();
+			this.bilinearAlgorythmButton = new System.Windows.Forms.RadioButton();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.pictureBox2.Location = new System.Drawing.Point(527, 12);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(605, 484);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pictureBox2.TabIndex = 1;
+			this.pictureBox2.TabStop = false;
+			this.pictureBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox2_MouseClick);
+			// 
+			// selectImageButton
+			// 
+			this.selectImageButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.selectImageButton.Location = new System.Drawing.Point(27, 26);
+			this.selectImageButton.Name = "selectImageButton";
+			this.selectImageButton.Size = new System.Drawing.Size(249, 44);
+			this.selectImageButton.TabIndex = 2;
+			this.selectImageButton.Text = "Select image";
+			this.selectImageButton.UseVisualStyleBackColor = true;
+			this.selectImageButton.Click += new System.EventHandler(this.SelectImageButton_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.pictureBox1.Location = new System.Drawing.Point(27, 146);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(451, 321);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseClick);
+			// 
+			// transformButton
+			// 
+			this.transformButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.transformButton.Location = new System.Drawing.Point(27, 91);
+			this.transformButton.Name = "transformButton";
+			this.transformButton.Size = new System.Drawing.Size(249, 32);
+			this.transformButton.TabIndex = 2;
+			this.transformButton.Text = "Make transformation";
+			this.transformButton.UseVisualStyleBackColor = true;
+			this.transformButton.Click += new System.EventHandler(this.TransformButton_Click);
+			// 
+			// applyFilterButton
+			// 
+			this.applyFilterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.applyFilterButton.Location = new System.Drawing.Point(282, 91);
+			this.applyFilterButton.Name = "applyFilterButton";
+			this.applyFilterButton.Size = new System.Drawing.Size(196, 32);
+			this.applyFilterButton.TabIndex = 2;
+			this.applyFilterButton.Text = "Apply filter";
+			this.applyFilterButton.UseVisualStyleBackColor = true;
+			this.applyFilterButton.Click += new System.EventHandler(this.ApplyFilterButton_Click);
+			// 
+			// filtersBox
+			// 
+			this.filtersBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.filtersBox.FormattingEnabled = true;
+			this.filtersBox.Location = new System.Drawing.Point(282, 46);
+			this.filtersBox.Name = "filtersBox";
+			this.filtersBox.Size = new System.Drawing.Size(196, 24);
+			this.filtersBox.TabIndex = 3;
+			// 
+			// primaryAlgorythmButtom
+			// 
+			this.primaryAlgorythmButtom.AutoSize = true;
+			this.primaryAlgorythmButtom.Location = new System.Drawing.Point(282, 26);
+			this.primaryAlgorythmButtom.Name = "primaryAlgorythmButtom";
+			this.primaryAlgorythmButtom.Size = new System.Drawing.Size(59, 17);
+			this.primaryAlgorythmButtom.TabIndex = 5;
+			this.primaryAlgorythmButtom.TabStop = true;
+			this.primaryAlgorythmButtom.Text = "Primary";
+			this.primaryAlgorythmButtom.UseVisualStyleBackColor = true;
+			this.primaryAlgorythmButtom.CheckedChanged += new System.EventHandler(this.PrimaryAlgorythmButtom_CheckedChanged);
+			// 
+			// bilinearAlgorythmButton
+			// 
+			this.bilinearAlgorythmButton.AutoSize = true;
+			this.bilinearAlgorythmButton.Location = new System.Drawing.Point(419, 26);
+			this.bilinearAlgorythmButton.Name = "bilinearAlgorythmButton";
+			this.bilinearAlgorythmButton.Size = new System.Drawing.Size(59, 17);
+			this.bilinearAlgorythmButton.TabIndex = 5;
+			this.bilinearAlgorythmButton.TabStop = true;
+			this.bilinearAlgorythmButton.Text = "Bileniar";
+			this.bilinearAlgorythmButton.UseVisualStyleBackColor = true;
+			this.bilinearAlgorythmButton.CheckedChanged += new System.EventHandler(this.BilinearAlgorythmButton_CheckedChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1301, 508);
+			this.Controls.Add(this.bilinearAlgorythmButton);
+			this.Controls.Add(this.primaryAlgorythmButtom);
+			this.Controls.Add(this.filtersBox);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.transformButton);
+			this.Controls.Add(this.applyFilterButton);
+			this.Controls.Add(this.selectImageButton);
+			this.Controls.Add(this.pictureBox2);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Button selectImageButton;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button transformButton;
+		private System.Windows.Forms.Button applyFilterButton;
+		private System.Windows.Forms.ComboBox filtersBox;
+		private System.Windows.Forms.RadioButton primaryAlgorythmButtom;
+		private System.Windows.Forms.RadioButton bilinearAlgorythmButton;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,403 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	enum Effects
+	{
+		Blur = 0,
+		Blur2 = 1,
+		Sharpen = 2,
+		MotionBlur = 3
+	}
+
+	enum Algorythm
+	{
+		Primary,
+		Bilinear
+	}
+
+	public partial class Form1 : Form
+	{
+		private Dictionary<Effects, string> effectsDict;
+
+		private readonly Point[] originalPoints = new Point[3];
+		private readonly Point[] newPoints = new Point[3];
+		private byte pointIndex = 0;
+		private readonly Graphics g1;
+		private readonly Graphics g2;
+		private Algorythm algorythm;
+
+		public Form1()
+		{
+			InitializeComponent();
+			g1 = pictureBox1.CreateGraphics();
+			g2 = pictureBox2.CreateGraphics();
+			InitializeDictionary();
+			InitializeComboBox();
+
+			// By default
+			algorythm = Algorythm.Primary;
+		}
+
+		private void InitializeDictionary()
+		{
+			effectsDict = new Dictionary<Effects, string>
+			{
+				// Remember the order
+				{ Effects.Blur, "Сильное размытие" },
+				{ Effects.Blur2, "Размытие" },
+				{ Effects.Sharpen, "Увеличение резкости" },
+				{ Effects.MotionBlur, "Размытие в движении" }
+			};
+		}
+
+		private void InitializeComboBox()
+		{
+			foreach (var effect in effectsDict.Values)
+			{
+				filtersBox.Items.Add(effect);
+			}
+		}
+
+		private int[,] SelectConvolutionMatrix()
+		{
+			//Repeat the order
+			switch (filtersBox.SelectedIndex)
+			{
+				case (int)Effects.Blur:
+					return new int[,]
+					{
+						{1, 1, 1 },
+						{1, 1, 1 },
+						{1, 1, 1 }
+					};
+				case (int)Effects.Blur2:
+					return new int[,]
+					{
+						{1, 2, 1 },
+						{2, 4, 2 },
+						{1, 2, 1 }
+					};
+				case (int)Effects.Sharpen:
+					return new int[,]
+					{
+						{-1, -1, -1 },
+						{-1, 9, -1 },
+						{-1, -1, -1 }
+					};
+				case (int)Effects.MotionBlur:
+					return new int[,]
+					{
+						{0, 0, 0 },
+						{5, 5, 5 },
+						{0, 0, 0 }
+					};
+
+				default:
+					return new int[,]
+					{
+						{0, 1, 0 },
+						{1, 0, -1 },
+						{0, -1, 0 }
+					};
+			}
+		}
+
+		private double CalculateSumOfMatrixElements(int[,] A)
+		{
+			var S = 0;
+			for (int i = 0; i < A.GetLength(0); i++)
+				for (int j = 0; j < A.GetLength(1); j++)
+					S += A[i, j];
+
+			return S;
+		}
+
+		private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (pointIndex == 6) pointIndex = 0;
+			// Get the coordinates of the clicked point
+			Point clickedPoint = e.Location;
+
+			if (pointIndex < 3)
+			{
+				originalPoints[pointIndex] = clickedPoint;
+
+				g1.FillEllipse(Brushes.Red, clickedPoint.X - 4, clickedPoint.Y - 4, 8, 8);
+				pointIndex++;
+			}
+			else MessageBox.Show("You already set 3 points");
+		}
+
+		private void PictureBox2_MouseClick(object sender, MouseEventArgs e)
+		{
+			// Get the coordinates of the clicked point
+			Point clickedPoint = e.Location;
+
+			if (pointIndex >=3 && pointIndex < 6)
+			{
+				newPoints[pointIndex % 3] = clickedPoint;
+
+				g2.FillEllipse(Brushes.Red, clickedPoint.X - 5, clickedPoint.Y - 5, 10, 10);
+				pointIndex++;
+			}
+			else if (pointIndex < 3)
+			{
+				MessageBox.Show("Before set 3 points on initial image!");
+			}
+			else
+			{
+				MessageBox.Show("You already set 3 points");
+
+			}
+		}
+
+		private void SelectImageButton_Click(object sender, EventArgs e)
+		{
+			pointIndex = 0;
+
+			OpenFileDialog openFileDialog = new OpenFileDialog
+			{
+				Filter = "Image Files (*.jpg; *.png; *.bmp)|*.jpg; *.png; *.bmp;"
+			};
+
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				string imagePath = openFileDialog.FileName;
+				try
+				{
+					// Load the image into the PictureBox
+					pictureBox1.Image = new Bitmap(imagePath);
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show("Error loading the image: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			}
+		}
+
+		private void TransformButton_Click(object sender, EventArgs e)
+		{
+			if (pictureBox1.Image == null)
+			{
+				MessageBox.Show("Before you need select an image");
+				return;
+			}
+
+			var transformationMatrix = CalculateTransformationMatrix();
+
+			Bitmap img1 = new Bitmap(pictureBox1.Image);
+			Bitmap img2;    // Output image
+
+			switch (algorythm)
+			{
+				case Algorythm.Primary:
+					img2 = GetPrimaryTransformedImage(transformationMatrix, img1);
+					break;
+				case Algorythm.Bilinear:
+					img2 = GetBilinearFilteringTransformedImage(transformationMatrix, img1);
+					break;
+				default:
+					throw new Exception("Unexisting algorythm");
+			}
+
+			pictureBox2.Image = img2;
+		}
+
+		private static Bitmap GetPrimaryTransformedImage(Matrix<double> transformationMatrix, Bitmap img1)
+		{
+			var img2 = new Bitmap(img1.Width*2, img1.Height*2);
+
+			var inversedTransformationMatrix = transformationMatrix.Inverse();
+			for (int x = 0; x < img2.Width; x++)
+			{
+				for (int y = 0; y < img2.Height; y++)
+				{
+					// Apply the reverse transformation to coordinates (X', Y')
+					Vector<double> outputCoordinates = Vector<double>.Build.DenseOfArray(new double[] { x, y, 1 });
+					Vector<double> inputCoordinates = inversedTransformationMatrix * outputCoordinates;
+
+					int inputX = (int)Math.Round(inputCoordinates[0]);
+					int inputY = (int)Math.Round(inputCoordinates[1]);
+
+					if (inputX >= 0 && inputX < img1.Width && inputY >= 0 && inputY < img1.Height)
+					{
+						// Get the source pixel and set it to resulting image
+						Color inputPixelColor = img1.GetPixel(inputX, inputY);
+						img2.SetPixel(x, y, inputPixelColor);
+					}
+					else
+					{
+						// Set black color, if pixel out of image
+						img2.SetPixel(x, y, Color.Black);
+					}
+				}
+			}
+
+			return img2;
+		}
+
+		private static Bitmap GetBilinearFilteringTransformedImage(Matrix<double> transformationMatrix, Bitmap img1)
+		{
+			var img2 = new Bitmap(img1.Width*2, img1.Height*2);
+
+			var inversedTransformationMatrix = transformationMatrix.Inverse();
+
+
+			for (int x = 0; x < img2.Width; x++)
+			{
+				for (int y = 0; y < img2.Height; y++)
+				{
+					// Применяем обратное преобразование к координатам (X', Y')
+					Vector<double> outputCoordinates = Vector<double>.Build.DenseOfArray(new double[] { x, y, 1 });
+					Vector<double> inputCoordinates = inversedTransformationMatrix * outputCoordinates;
+
+					var inputX = inputCoordinates[0];
+					var inputY = inputCoordinates[1];
+
+					// Calculating coordinates of 4 neighboring points
+					int XUp, XDown;
+					int YUp, YDown;
+					XDown = (int)Math.Floor(inputX);
+					XUp = (int)Math.Ceiling(inputX);
+					YDown = (int)Math.Floor(inputY);
+					YUp = (int)Math.Ceiling(inputY);
+
+					if (XUp >= 0 && XUp < img1.Width && YUp >= 0 && YUp < img1.Height
+						&& XDown >= 0 && XDown < img1.Width && YDown >= 0 && YDown < img1.Height)
+					{
+						// Calculating color of point
+						Color color = GetPixelColor(XUp, XDown, YUp, YDown, inputX, inputY, img1);
+						img2.SetPixel(x, y, color);
+					}
+					else
+					{
+						img2.SetPixel(x, y, Color.Black);
+					}
+				}
+			}
+			return img2;
+		}
+
+		// Get pixel in algorythm with bilinear filtering
+		private static Color GetPixelColor(int xUp, int xDown, int yUp, int yDown, double x, double y, Bitmap img1)
+		{
+			Color c11 = img1.GetPixel(xDown, yUp);
+			Color c12 = img1.GetPixel(xUp, yUp);
+			Color c21 = img1.GetPixel(xDown, yDown);
+			Color c22 = img1.GetPixel(xUp, yDown);
+
+			int sumR, sumG, sumB;
+
+			double w1 = (xUp - x);
+			double w2 = (x - xDown);
+			double w3 = (yUp - y);
+			double w4 = (y - yDown);
+
+			sumR = (int)((c21.R * w1 + c22.R * w2) * w3 + (c11.R * w1 + c12.R * w2) * w4);
+			sumG = (int)((c21.G * w1 + c22.G * w2) * w3 + (c11.G * w1 + c12.G * w2) * w4);
+			sumB = (int)((c21.B * w1 + c22.B * w2) * w3 + (c11.B * w1 + c12.B * w2) * w4);
+
+			if (sumR > 255) sumR = 255;
+			if (sumG > 255) sumG = 255;
+			if (sumB > 255) sumB = 255;
+
+			return Color.FromArgb(sumR, sumG, sumB);
+		}
+
+		private Matrix<double> CalculateTransformationMatrix()
+		{
+			// Creating matrix from source points
+			var matrixA = Matrix<double>.Build.DenseOfArray(new double[,] {
+			{ originalPoints[0].X, originalPoints[0].Y, 1 },
+			{ originalPoints[1].X, originalPoints[1].Y, 1 },
+			{ originalPoints[2].X, originalPoints[2].Y, 1 }
+			});
+
+			// Creating matrix from result points
+			var matrixB = Matrix<double>.Build.DenseOfArray(new double[,] {
+			{ newPoints[0].X, newPoints[0].Y, 1 },
+			{ newPoints[1].X, newPoints[1].Y, 1 },
+			{ newPoints[2].X, newPoints[2].Y, 1 }
+			});
+
+			// Solve system Ax = B 
+			var x = matrixA.Solve(matrixB);
+
+			var transformationMatrix = Matrix<double>.Build.DenseOfArray(new double[,] {
+				{ x[0, 0], x[1, 0], x[2, 0] },
+				{ x[0, 1], x[1, 1], x[2, 1] },
+				{ x[0, 2], x[1, 2], x[2, 2] },
+			});
+
+			return transformationMatrix;
+		}
+
+		private void ApplyFilterButton_Click(object sender, EventArgs e)
+		{
+			// Convolution matrix
+			var A = SelectConvolutionMatrix();
+
+			var img1 = new Bitmap(pictureBox1.Image);
+			Bitmap img2;
+			double S = CalculateSumOfMatrixElements(A); // Sum of matrix elements
+
+			img2 = GetFilteredImage(A, img1, S);
+
+			pictureBox2.Image = img2;
+		}
+
+		private static Bitmap GetFilteredImage(int[,] A, Bitmap img1, double S)
+		{
+			int sumR, sumG, sumB;
+			Color color;
+
+			var img2 = new Bitmap(img1.Width, img1.Height);
+
+			for (int x = 1; x < img2.Width - 1; x++)
+			{
+				for (int y = 1; y < img2.Height - 1; y++)
+				{
+					sumR = sumG = sumB = 0;
+					for (int k1 = x - 1; k1 <= x + 1; k1++)
+						for (int k2 = y - 1; k2 <= y + 1; k2++)
+						{
+							color = img1.GetPixel(k1, k2);
+
+							sumR += color.R * A[k1 - x + 1, k2 - y + 1];
+							sumB += color.B * A[k1 - x + 1, k2 - y + 1];
+							sumG += color.G * A[k1 - x + 1, k2 - y + 1];
+						}
+
+					sumR = (int)Math.Round(sumR / S);
+					if (sumR < 0) sumR = 0; if (sumR > 255) sumR = 255;
+
+					sumG = (int)Math.Round(sumG / S);
+					if (sumG < 0) sumG = 0; if (sumG > 255) sumG = 255;
+
+					sumB = (int)Math.Round(sumB / S);
+					if (sumB < 0) sumB = 0; if (sumB > 255) sumB = 255;
+
+					Color newColor = Color.FromArgb(sumR, sumG, sumB);
+					img2.SetPixel(x, y, newColor);
+				}
+			}
+
+			return img2;
+		}
+
+		private void PrimaryAlgorythmButtom_CheckedChanged(object sender, EventArgs e)
+		{
+			algorythm = Algorythm.Primary;
+		}
+
+		private void BilinearAlgorythmButton_CheckedChanged(object sender, EventArgs e)
+		{
+			algorythm = Algorythm.Bilinear;
+		}
+	}
+}
